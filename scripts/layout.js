0be57543c97f94ae41e3a3cfd4b74b76de226e0e
Matthew Butt <matthew.butt@7digital.com>
2012-01-19 14:29:27 +0000

    It seems to work better if I just round the angle of rotation; not the angle used for positioning

--- scripts/layout.js
+++ scripts/layout.js
@@ -8,9 +8,9 @@
 
 	for (i = 0; i < totalSteps; i = i + 1) {
 		step = steps[i];
-		angle = Math.round(i * 360 / totalSteps);
+		angle = i * 360 / totalSteps;
 		step.setAttribute("data-x", 3000 * Math.cos(Math.PI * angle / 180));
 		step.setAttribute("data-y", 3000 * Math.sin(Math.PI * angle / 180));
-		step.setAttribute("data-rotate", angle);
+		step.setAttribute("data-rotate", Math.round(angle));
 	}
 }());
