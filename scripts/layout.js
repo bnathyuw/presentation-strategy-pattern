6659fae2d21b07a7d99568178ff2689e8e09c4db
Matthew Butt <matthew.butt@7digital.com>
2012-01-19 14:39:34 +0000

    More changes to the layout algorithm.

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,16 +1,18 @@
 (function () {
 	"use strict";
-	var steps, totalSteps, i, step, angle;
+	var steps, totalSteps, i, step, angle, radius;
 
 	steps = document.querySelectorAll(".step");
 
 	totalSteps = steps.length;
 
+	radius = (totalSteps * steps[0].clientHeight * 1.3) / (2 * Math.PI);
+
 	for (i = 0; i < totalSteps; i = i + 1) {
 		step = steps[i];
 		angle = i * 360 / totalSteps;
-		step.setAttribute("data-x", 3000 * Math.cos(Math.PI * angle / 180));
-		step.setAttribute("data-y", 3000 * Math.sin(Math.PI * angle / 180));
+		step.setAttribute("data-x", radius * Math.cos(Math.PI * angle / 180));
+		step.setAttribute("data-y", radius * Math.sin(Math.PI * angle / 180));
 		step.setAttribute("data-rotate", Math.round(angle));
 	}
 }());
